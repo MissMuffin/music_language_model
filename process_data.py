4b329f7c0ae982aedb69546c35e08078f6d51274
Miss Muffin <fyyree@gmail.com>
2019-10-12 12:10:14 +0200

    added processing data for multiple datasets with option to merge them

--- process_data.py
+++ process_data.py
@@ -38,29 +38,56 @@ def create_folders(data_raw, data_processed):
         os.makedirs(f"{chordwise}/test")
         os.makedirs(f"{chordwise}/valid")
 
-def encode_data(data_raw):
+def create_merged_folders(data_raw, data_processed):
+    # create processed data dir
+    if not os.path.exists(data_processed):
+        os.makedirs(data_processed)
+    else:
+        clear_folder(data_processed)
+
+        # create notewise and chordwise dirs with test, train, valid in each
+        notewise = f"{data_processed}/notewise"
+        chordwise = f"{data_processed}/chordwise"
+
+        os.makedirs(f"{notewise}/train")
+        os.makedirs(f"{notewise}/test")
+        os.makedirs(f"{notewise}/valid")
+
+        os.makedirs(f"{chordwise}/train")
+        os.makedirs(f"{chordwise}/test")
+        os.makedirs(f"{chordwise}/valid")
+
+def encode_data(data_raw, merged=True):
     # TODO add progress bar
+    # we assume a dataset always has only train, test, valid folders
     for dataset in os.listdir(data_raw):
-        # we assume a dataset always has only train, test, valid folders
         dataset_path = f"{data_raw}/{dataset}"
+
         for folder in os.listdir(dataset_path):
             folder_path = f"{dataset_path}/{folder}"
+
             for fname in os.listdir(folder_path):
                 file_path = f"{folder_path}/{fname}"
 
                 fname = os.path.splitext(fname)[0]
                 chords, notes = translate_piece(file_path)
-                with open(f"{data_processed}/{dataset}/chordwise/{folder}/{fname}.txt", "w") as fn:
+
+                write_location = f"{data_processed}"
+                if not merged:
+                    write_location = f"{data_processed}/{dataset}"
+
+                with open(f"{write_location}/chordwise/{folder}/{fname}.txt", "w") as fn:
                     fn.write(chords)
-                with open(f"{data_processed}/{dataset}/notewise/{folder}/{fname}.txt", "w") as fn:
+                with open(f"{write_location}/notewise/{folder}/{fname}.txt", "w") as fn:
                     fn.write(notes)
+                break
 
 if __name__ == "__main__":
 
     data_raw = "data"
     data_processed = "processed_data"
 
-    create_folders(data_raw, data_processed)
-    encode_data(data_raw)
+    create_merged_folders(data_raw, data_processed)
+    encode_data(data_raw, merged=True)
 
     print("All files encoded")
